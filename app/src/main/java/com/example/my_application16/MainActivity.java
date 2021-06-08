8b19aa2d6ec346e78d8948898012fbd4ce389686
lamyita <50775487+lamyita@users.noreply.github.com>
2021-06-08 15:06:42 +0100

    fin api

--- app/src/main/java/com/example/my_application16/MainActivity.java
+++ app/src/main/java/com/example/my_application16/MainActivity.java
@@ -4,27 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     Button button, button2, button3;
     EditText name;
+    ListView lst;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
         button3 = findViewById(R.id.button3);
 
         name = findViewById(R.id.Name);
+
+        lst = findViewById(R.id.list);
 
         final WeatherDataService weatherDataService = new WeatherDataService(MainActivity.this);
 
@@ -57,6 +52,52 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
 
+            }
+        });
+
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                weatherDataService.getCityForecastByID(name.getText().toString(), new WeatherDataService.ForeCastByIDResponse() {
+                    @Override
+                    public void onError(String message) {
+
+                    }
+
+                    @Override
+                    public void onResponce(List<WeatherReportModel> weatherReportModels) {
+
+                        /////put the entire list into the listVew control
+                        ArrayAdapter arrayAdapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, weatherReportModels);
+                        lst.setAdapter(arrayAdapter);
+                    }
+                });
+
+
+
+                }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                weatherDataService.getCityforecasByName(name.getText().toString(), new WeatherDataService.GetCityForecastByNameCallback() {
+                    @Override
+                    public void onError(String message) {
+                        Toast.makeText(MainActivity.this, "Somethong wrong", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onResponse(List<WeatherReportModel> weatherReportModels) {
+                        /////put the entire list into the listVew control
+                        ArrayAdapter arrayAdapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, weatherReportModels);
+                        lst.setAdapter(arrayAdapter);
+                    }
+                });
             }
         });
     }
